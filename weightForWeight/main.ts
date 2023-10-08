c14e49ffc23f73f8feda2fe116ccf526b8fa3384
mrbaggiebug <mani.ehsanisohi@gmail.com>
2023-10-08 10:54:48 +0330

    I THINK THIS IS IT

--- weightForWeight/main.ts
+++ weightForWeight/main.ts
@@ -1,51 +1,42 @@
 function orderWeight(strng: string): string {
 
-    const inputArr: number[] = strng.split(" ").map((x) => {
+    let resultArr: string[] = []
+    let inputArr: number[] = strng.split(" ").map((x) => {
 
         return parseInt(x)
     })
 
-    const digitSumArray: number[] = inputArr.map((x) => {
+    let digitSumArray: number[] = inputArr.map((x) => {
 
         let acc = 0
         let digits = x.toString().split("").map((x) => {
             return parseInt(x)
         })
 
-        // console.log(digits)
-
         for (let i in digits) {
             acc += digits[i]
         }
-
         return acc
     })
 
-    // let mergedArray: any = () => {
-
-    //     // this is just a key value array
-    //     // console.log(inputArr, digitSumArray)
-
-    //     let resultArr: number[][] = []
-    //     for (let i in inputArr) {
-    //         resultArr.push([inputArr[i], digitSumArray[i]])
-    //     }
-    //     console.log(resultArr)
-    //     return resultArr
-    // }
-
-    console.log({ digitSumArray })
+    // This line below decreased my life expectancy by 8 years and my will to live by 90%
+    let sortedDigitArray = sortArray([...digitSumArray])
     let dictionary = new Map()
 
     for (let i in inputArr) {
-        dictionary.set(inputArr[i], digitSumArray[i])
+        dictionary.set(digitSumArray[i], inputArr[i])
     }
 
-    console.log({ inputArr, digitSumArray })
+    for (let i in sortedDigitArray) {
+        resultArr.push(dictionary.get(sortedDigitArray[i]))
+    }
+
+    console.log({ inputArr, digitSumArray, sortedDigitArray, resultArr })
+    return resultArr.map((x) => { return x.toString() }).join(" ")
 }
 
 function sortArray(arr: number[]): number[] {
-    return arr.sort(function (a, b) { return b - a })
+    return arr = arr.sort(function (a, b) { return b - a })
 }
 
 orderWeight("103 123 4444 99 2000")
